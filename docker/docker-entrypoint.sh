4528f6f436243cb7d6c2683b79624c9dcda8b050
Nick Ballenger <nballeng@gmail.com>
2021-04-05 11:55:25 -0400

    Making the docker entrypoint rwait on pg startup

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,31 @@
-#! /usr/bin/env sh
+#!/bin/sh
 set -e
+
+PG_READY=""
+PG_READY_WAIT_SECONDS=5
+COUNT=0
+
+cd /libreg_app
+
+until [ -n "$PG_READY" ] || [ $COUNT -gt 5 ]; do
+    COUNT=$((COUNT+1))
+
+    pipenv run python > /dev/null 2>&1 <<EOF
+import os,sys,psycopg2
+try:
+  psycopg2.connect(os.environ.get('SIMPLIFIED_PRODUCTION_DATABASE'))
+except Exception:
+  sys.exit(1)
+sys.exit(0)
+EOF
+
+    if [ $? -eq 0 ]; then
+        PG_READY="true"
+    else
+        echo "--- Postgres unavailable, sleeping $PG_READY_WAIT_SECONDS seconds"
+        sleep $PG_READY_WAIT_SECONDS
+    fi
+done
 
 # Start up Supervisor, with Nginx and uWSGI
 exec /usr/local/bin/supervisord -c /etc/supervisord.conf
