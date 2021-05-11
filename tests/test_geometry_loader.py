8aa10e02fda0b1c8fd8e9cd0edecf4c6b6538be6
Nick Ballenger <nballeng@gmail.com>
2021-05-11 14:29:44 -0400

    flake8 changes to geometryloader

--- tests/test_geometry_loader.py
+++ tests/test_geometry_loader.py
@@ -1,9 +1,7 @@
 from io import StringIO
 
 from sqlalchemy import func
-from geoalchemy2 import Geography
 from model import (
-    get_one,
     get_one_or_create,
     Place,
     PlaceAlias,
@@ -24,8 +22,8 @@ class TestGeometryLoader(DatabaseTest):
 
     def test_load(self):
         # Load a place identified by a GeoJSON Polygon.
-        metadata = '{"parent_id": null, "name": "77977", "id": "77977", "type": "postal_code", "aliases": [{"name": "The 977", "language": "eng"}]}'
-        geography = '{"type": "Polygon", "coordinates": [[[-96.840066, 28.683039], [-96.830637, 28.690131], [-96.835048, 28.693599], [-96.833515, 28.694926], [-96.82657, 28.699584], [-96.822495, 28.695826], [-96.821248, 28.696391], [-96.814249, 28.700983], [-96.772337, 28.722765], [-96.768804, 28.725363], [-96.768564, 28.725046], [-96.767246, 28.723276], [-96.765295, 28.722084], [-96.764568, 28.720456], [-96.76254, 28.718483], [-96.763087, 28.717521], [-96.761814, 28.716488], [-96.761088, 28.713623], [-96.762231, 28.712798], [-96.75967, 28.709812], [-96.781093, 28.677548], [-96.784803, 28.675363], [-96.793788, 28.669546], [-96.791527, 28.667603], [-96.808567, 28.678507], [-96.81505, 28.682946], [-96.820191, 28.684517], [-96.827178, 28.679867], [-96.828626, 28.681719], [-96.831309, 28.680451], [-96.83565, 28.677724], [-96.840066, 28.683039]]]}'
+        metadata = '{"parent_id": null, "name": "77977", "id": "77977", "type": "postal_code", "aliases": [{"name": "The 977", "language": "eng"}]}'  # noqa: E501
+        geography = '{"type": "Polygon", "coordinates": [[[-96.840066, 28.683039], [-96.830637, 28.690131], [-96.835048, 28.693599], [-96.833515, 28.694926], [-96.82657, 28.699584], [-96.822495, 28.695826], [-96.821248, 28.696391], [-96.814249, 28.700983], [-96.772337, 28.722765], [-96.768804, 28.725363], [-96.768564, 28.725046], [-96.767246, 28.723276], [-96.765295, 28.722084], [-96.764568, 28.720456], [-96.76254, 28.718483], [-96.763087, 28.717521], [-96.761814, 28.716488], [-96.761088, 28.713623], [-96.762231, 28.712798], [-96.75967, 28.709812], [-96.781093, 28.677548], [-96.784803, 28.675363], [-96.793788, 28.669546], [-96.791527, 28.667603], [-96.808567, 28.678507], [-96.81505, 28.682946], [-96.820191, 28.684517], [-96.827178, 28.679867], [-96.828626, 28.681719], [-96.831309, 28.680451], [-96.83565, 28.677724], [-96.840066, 28.683039]]]}'  # noqa: E501
         texas_zip, is_new = self.loader.load(metadata, geography)
         assert is_new is True
         assert texas_zip.external_id == "77977"
@@ -38,7 +36,7 @@ class TestGeometryLoader(DatabaseTest):
         assert alias.language == "eng"
 
         # Load another place identified by a GeoJSON Point.
-        metadata = '{"parent_id": null, "name": "New York", "type": "state", "abbreviated_name": "NY", "id": "NY", "full_name": "New York", "aliases": [{"name": "New York State", "language": "eng"}]}'
+        metadata = '{"parent_id": null, "name": "New York", "type": "state", "abbreviated_name": "NY", "id": "NY", "full_name": "New York", "aliases": [{"name": "New York State", "language": "eng"}]}'  # noqa: E501
         geography = '{"type": "Point", "coordinates": [-75, 43]}'
         new_york, is_new = self.loader.load(metadata, geography)
         assert new_york.abbreviated_name == "NY"
@@ -81,8 +79,7 @@ class TestGeometryLoader(DatabaseTest):
         )
         old_us_geography = old_us.geometry
 
-        # Load a small NDJSON "file" containing information about
-        # three places.
+        # Load a small NDJSON "file" containing information about three places.
         test_ndjson = """{"parent_id": null, "name": "United States", "aliases": [{"name" : "The Good Old U. S. of A.", "language": "eng"}], "type": "nation", "abbreviated_name": "US", "id": "US"}
 {"type": "Point", "coordinates": [-159.459551, 54.948652]}
 {"parent_id": "US", "name": "Alabama", "aliases": [], "type": "state", "abbreviated_name": "AL", "id": "01"}
